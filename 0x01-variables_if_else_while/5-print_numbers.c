ac772c221632b10c992995bbc93b93075040039b
Awini Maxwell <awinimaxwell428@gmail.com>
2023-02-20 18:43:33 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints integers from 1 to 10 inclusive
+ * Return: (0) (success)
+ */
+int main(void)
+{
+	char num;
+
+	for (num = 0; num <= 9; num++)
+	printf("%d", num);
+	printf("\n");
+	return (0);
+}
