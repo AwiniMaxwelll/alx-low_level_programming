98b8a1ce033d1c91219d9ed6df1c98683d258198
Awini Maxwell <awinimaxwell428@gmail.com>
2023-02-23 21:23:45 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 14, 10 times
+ */
+void print_most_numbers(void)
+{
+int i;
+int d = 0;
+
+while (d < 14)
+{
+for (i = 0; i <= 14; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+d++
+}
+}
