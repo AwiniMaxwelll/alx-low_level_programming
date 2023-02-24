9f657297d585c977f2c70811f61de2c1f1fe1006
Awini Maxwell <awinimaxwell428@gmail.com>
2023-02-23 20:57:32 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+int i;
+int d = 0;
+
+while (d < 14)
+{
+for (i = 0; i <= 9; i++)
+{
+if (i != 2 && i != 4)
+_putchar(i + '0');
+}
+d++;
+_putchar('\n');
+}
+}
