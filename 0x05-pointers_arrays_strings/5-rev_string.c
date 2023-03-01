efedc063153fd930dd287442b037b9b5e8b66e41
Awini Maxwell <awinimaxwell428@gmail.com>
2023-02-28 20:22:57 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string in place
+ * @s: the string to reverse
+ */
+void rev_string(char *s)
+{
+int len = 0, i;
+char swaper;
+
+while (s[len] != '\0')
+len++;
+
+for (i = 0; i < len / 2; i++)
+{
+swaper = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = swaper;
+}
+}
+
