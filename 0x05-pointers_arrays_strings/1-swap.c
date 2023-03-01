a938564fc13d09fa4ee242556365e874a1fb6b7a
Awini Maxwell <awinimaxwell428@gmail.com>
2023-02-28 18:17:12 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * swap_int - swaps the value of a and b
- * @a: is an input
- * @b: is an input
+ * @a: is an integer be swaped
+ * @b: is an integer to be swaped
  * Return: return the value of a as b and b as a
  */
 void swap_int(int *a, int *b)
 {
-int c = *a;
+int c;
+c = *a;
 *a = *b;
-*b = *c;
+*b = c;
 }
