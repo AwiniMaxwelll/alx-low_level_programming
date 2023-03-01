f46dc99e2938c628c90df2340be9799b0dc691f5
Awini Maxwell <awinimaxwell428@gmail.com>
2023-02-28 18:30:40 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to find the length of
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+int lenght = 0;
+while (*s != '\0')
+{
+lenght++;
+s++;
+}
+return (lenght);
+}
