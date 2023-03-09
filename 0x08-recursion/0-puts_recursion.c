cf3eb40668f3bcd860c5fe3e2f0cbda04cab3df0
Awini Maxwell <awinimaxwell428@gmail.com>
2023-03-09 02:37:27 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a newline
+ * @s : the character to be printed
+ * Return: returns void
+ */
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s)
+_putchar(s + 1)
+}
