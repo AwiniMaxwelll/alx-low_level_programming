7a95f82944b6cc2ec3581ab8f324af9545c1f263
Awini Maxwell <awinimaxwell428@gmail.com>
2023-03-09 09:45:47 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include  "main.h"
+/**
+ *_print_rev_recursion-print the reverse of a string, ending with a new line
+ *@s: the string character to be reversed
+ *Return: returns void
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+else
+{
+_putchar('\n');
+}
+}
