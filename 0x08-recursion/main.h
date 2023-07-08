00a77673e28bfc645626a48c766d9b4b336f682a
AwiniMaxwelll <AwiniMaxwell428@gmail.com>
2023-07-08 06:12:13 -0700

    _division_checker is added

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,4 +9,6 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int _putchar(char c);
+int _division_checker(int n, int div);
+void _print_rev_recursion(char *s);
 #endif
