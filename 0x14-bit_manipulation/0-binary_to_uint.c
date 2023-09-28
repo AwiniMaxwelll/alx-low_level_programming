024e3134e4260fd77ef0b07cf0c3268fa117cdb2
AwiniMaxwelll <awinimaxwell428@gmail.com>
2023-09-28 09:05:09 -0700

    corrections
    the loop started from 1 now changed to 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,9 +11,9 @@ unsigned int decimal_number = 0;
 int i;
 if (!b)
 return (0);
-for (i = 1; b[i]; i++)
+for (i = 0; b[i]; i++)
 {
-if (b[i] > '1' || b[i] < '0')
+if (b[i] > '1' || b[i] < '0' || b[i]== '-')
 {
 return (0);
 }
